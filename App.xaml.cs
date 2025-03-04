8bdc706d6ba3ebb5c85767d5badac0bcd03d02e6
Marco <marco.wkti@gmail.com>
2025-03-04 19:11:53 -0300

    Agenda 2

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,7 @@
             //MainPage = new AppShell();
             //new NavigationPage altera a página home, nova Home é ListaProdutos da pasta Views
             MainPage = new NavigationPage(new Views.ListaProduto());
+
         }
     }
 }
