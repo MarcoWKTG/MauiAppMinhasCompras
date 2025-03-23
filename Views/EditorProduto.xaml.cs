f79428daa211ea1e4364d646695d75fea41b6d76
Marco <marco.wkti@gmail.com>
2025-03-23 16:26:37 -0300

    Agenda 5 DS III

--- Views/EditorProduto.xaml.cs
+++ Views/EditorProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class EditorProduto : ContentPage
@@ -6,4 +8,33 @@ public partial class EditorProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e) //método para atualizar os dados do produto
+    {
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+            //passando os atributos do objeto criado
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_descricao.Text,
+                //atributo do tipo texto convertido para double
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            //acessando o método update e passando o objeto
+            await App.Db.Update(p);
+            //informando que foi salvo com sucesso
+            await DisplayAlert("Sucesso!", "Registro Atualizado", "Ok");
+            //retorna para a tela anterior
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)//capitura exceção
+        {
+            //informa sobre a exceção
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
--- Views/ListaProduto.xaml.cs
+++ Views/ListaProduto.xaml.cs
@@ -5,60 +5,111 @@ using System.Threading.Tasks;
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
-{	
-	
-	ObservableCollection<Produto> lista = new ObservableCollection<Produto>(); 
+{
 
-	public ListaProduto()
-	{
-		InitializeComponent();
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 
-		//todos os produtos da ObservableCollection irá para a lst_produtos
-		lst_produtos.ItemsSource = lista;
-	}
+    public ListaProduto()
+    {
+        InitializeComponent();
+
+        //todos os produtos da ObservableCollection irá para a lst_produtos
+        lst_produtos.ItemsSource = lista;
+    }
 
     protected async override void OnAppearing()
     {//exibe a lista de produtos
-        List<Produto> tmp = await App.Db.GetAll();
-		tmp.ForEach(i => lista.Add(i));
+        try
+        {
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
     }
 
     //Evento acionado após clicar no botão adicionar
     private void Adicionar_Clicked(object sender, EventArgs e)
     {
-		//verifica se ocorre alguma exceção
-		try
-		{
-			//redireciona para a página NovoProduto
-			Navigation.PushAsync(new Views.NovoProduto());
+        //verifica se ocorre alguma exceção
+        try
+        {
+            //redireciona para a página NovoProduto
+            Navigation.PushAsync(new Views.NovoProduto());
 
-		} catch (Exception ex)
-		{
-			DisplayAlert("Ops", ex.Message, "OK");
-		}
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 
     private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
     {
-		string q = e.NewTextValue;
+        try
+        {
+            string q = e.NewTextValue;
 
-		lista.Clear();
+            lista.Clear();
 
-		List<Produto> tmp = await App.Db.Search(q);
+            List<Produto> tmp = await App.Db.Search(q);
 
-		tmp.ForEach(i => lista.Add(i));
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
     }
 
     private void Somar_Clicked(object sender, EventArgs e)
     {
-		double soma = lista.Sum(i => i.Total);
+        double soma = lista.Sum(i => i.Total);
 
-		string msg = $"O total é {soma:C}";
-		DisplayAlert("Total dos Produtos", msg, "OK");
+        string msg = $"O total é {soma:C}";
+        DisplayAlert("Total dos Produtos", msg, "OK");
     }
 
-    private void Remover_Clicked(object sender, EventArgs e)
+    private async void Remover_Clicked(object sender, EventArgs e)
     {
+        try
+        {
+            MenuItem selecionado = sender as MenuItem; //produto selecionado
 
+            Produto p = selecionado.BindingContext as Produto;
+
+            bool confirm = await DisplayAlert(
+                "Tem Certeza?", $"Remover {p.Descricao}?", "Sim", "Não"); //solicita confirmação do usuário para excluir
+
+            if (confirm)//remove o item selecionado do BD e ObservableCollection
+            {
+                await App.Db.Delete(p.Id);
+                lista.Remove(p);
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e) //edita produto
+    {
+        try
+        {
+            Produto p = e.SelectedItem as Produto; //produto selecionado
+
+            Navigation.PushAsync(new Views.EditorProduto
+            {
+                BindingContext = p
+            });
+        }
+        catch(Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK");
+        }
     }
 }
